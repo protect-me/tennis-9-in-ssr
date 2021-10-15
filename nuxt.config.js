eb1b37290aa8d17805fd4be6fe2153d37dc3965a
protect-me <pmfwiww@gmail.com>
2021-10-15 09:45:34 +0900

    Feat: create project

---
+++ nuxt.config.js
@@ -0,0 +1,82 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - tennis-9-in-ssr',
+    title: 'tennis-9-in-ssr',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ['@nuxtjs/firebase', '@nuxtjs/dotenv'],
+
+  firebase: {
+    config: {
+      // REQUIRED: Official config for firebase.initializeApp(config):
+      apiKey: process.env.VUE_APP_apiKey,
+      authDomain: process.env.VUE_APP_authDomain,
+      databaseURL: process.env.VUE_APP_databaseURL,
+      projectId: process.env.VUE_APP_projectId,
+      storageBucket: process.env.VUE_APP_storageBucket,
+      messagingSenderId: process.env.VUE_APP_messagingSenderId,
+      appId: process.env.VUE_APP_appId,
+      measurementId: process.env.VUE_APP_measurementId,
+    },
+    services: {
+      auth: true,
+      firestore: true,
+      functions: true,
+      storage: true,
+      database: true,
+      analytics: true,
+      remoteConfig: true,
+    },
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
